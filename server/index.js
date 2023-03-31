d5b883aa1ed6190c6ebd0737b1f99da0d4180322
AB <abezguzov@gmail.com>
2023-03-31 23:51:32 +0200

    Add mongodb

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 
+import connectDB from "./mongodb/connect.js";
+
 dotenv.config();
 const app = express();
 app.use(cors());
@@ -12,9 +14,14 @@ app.get("/", async (req, res) => {
 });
 
 const startServer = async () => {
-  app.listen(8080, () =>
-    console.log("Server has started on port http://localhost:8080")
-  );
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () =>
+      console.log("Server has started on port http://localhost:8080")
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 startServer();
